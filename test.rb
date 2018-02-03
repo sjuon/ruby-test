c6612633b243f3e84b4de8749731bc6b4d8d4c28
sjuon <sjuon@me.com>
2018-02-03 16:07:58 +0100

    add as second file

---
+++ test.rb
@@ -0,0 +1,8 @@
+print "what is your name? "
+name = gets.chomp
+print "what is your age? "
+age = gets.chomp
+print "what is your favourite color? "
+color = gets.chomp
+
+puts "hello world. I am #{name}, I am #{age} years old and my favourite color is #{color}"
